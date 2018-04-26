d8b6b9c1c0ffdc4a69be65f693428a2444829d5c
Cofresi <cofresi@dash.org>
2018-04-26 17:00:53 -0400

    wip: ES6 syntax

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ async function main() {
 	try {
 		const orbitdb = new OrbitDB(ipfsapi('127.0.0.1', 5001));
 		const kvs = await orbitdb.kvstore('mynamespace');
-		const hash = await kvs.set('hello', { name: 'World' });
-		console.log('hash', hash);
+		let hash;
+		let counter = 0;
+		const wait = setInterval(async () => {
+			if (counter >= 10) {
+			clearInterval(wait);
+		}
+	else {
+			hash = await kvs.set(`hello ${counter}`, { name: `World ${counter}`});
+			console.log('hash', hash);
+			counter+=1;
+		}
+	}, 2000);
 	} catch(err) {
 		console.log(err);
 	}
