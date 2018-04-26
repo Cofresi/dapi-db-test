a95d44f82554755bef8b53fcacba99221a0c98db
Cofresi <cofresi@dash.org>
2018-04-26 17:44:10 -0400

    wip: active/passive

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const ipfsapi = require('ipfs-api');
 const	OrbitDB = require('orbit-db');
+const	parseArgs = require('minimist')
 
 main();
 
 async function main() {
 	try {
+		const opts={ boolean : true }
+		const argv = parseArgs(process.argv.slice(2), opts);
+		const isActive = Boolean(argv.true);
 		const orbitdb = new OrbitDB(ipfsapi('127.0.0.1', 5001));
 		const access = {
 			// Give write access to everyone
 			write: ['*'],
 		}
 		const kvs = await orbitdb.kvstore('mynamespace', access);
-		let hash;
-		let counter = 0;
-		const wait = setInterval(async () => {
-			if (counter >= 10) {
-			clearInterval(wait);
+		if(isActive) {
+			let hash;
+			let counter = 0;
+			const wait = setInterval(async () => {
+				if (counter >= 10) {
+					clearInterval(wait);
+				}
+				else {
+					hash = await kvs.set(`hello ${counter}`, { name: `World ${counter}`});
+					console.log('hash', hash);
+					counter+=1;
+				}
+			}, 2000);
 		}
-	else {
-			hash = await kvs.set(`hello ${counter}`, { name: `World ${counter}`});
-			console.log('hash', hash);
-			counter+=1;
-		}
-	}, 2000);
 	} catch(err) {
 		console.log(err);
 	}
